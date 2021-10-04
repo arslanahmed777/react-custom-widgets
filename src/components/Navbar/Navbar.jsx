45db51230691051c18155932a6aa0f1742711410
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2021-10-04 12:53:04 +0500

    downgrade to react-icons and worded on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Navbarcss from "./Navbar.module.css";
-
+import { FaBars } from "react-icons/fa";
 const Navbar = () => {
+  const [sidebar, setSideBar] = useState(true);
   return (
-    <div className={Navbarcss.navbar}>
-      {" "}
-      <Link to="/pagination" className={Navbarcss.menu}>
-        Pagination
-      </Link>
-      <Link to="/" className={Navbarcss.menu}>
-        Home
-      </Link>
-    </div>
+    <>
+      <div className={Navbarcss.navbar}>
+        <Link to="/pagination" className={Navbarcss.menu_bars}>
+          <FaBars />
+        </Link>
+      </div>
+      <nav
+        className={
+          sidebar
+            ? `${Navbarcss.navmenu} ${Navbarcss.active}`
+            : Navbarcss.navmenu
+        }
+      ></nav>
+    </>
   );
 };
 
