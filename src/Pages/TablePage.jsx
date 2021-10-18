8a50a2d09901fab61695ab6445230fc528696910
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2021-10-18 11:03:20 +0500

    RUMANA MUBARRUM
    RUMANA MUBARRUM

--- src/Pages/TablePage.jsx
+++ src/Pages/TablePage.jsx
@@ -1,49 +1,74 @@
-import React, { useState } from 'react'
-import Table from '../components/Table/Table'
-import Mock_Data from "../utils/data.json"
+import React, { useState } from "react";
+import Table from "../components/Table/Table";
+import Mock_Data from "../utils/data.json";
+
+import { useAsyncDebounce } from "react-table";
+
+export const GlobalFilter = ({ filter, setFilter }) => {
+  const [value, setValue] = useState(filter);
+
+  const onChanges = useAsyncDebounce((value) => {
+    setFilter(value || undefined);
+  }, 1000);
+  return (
+    <div>
+      <input
+        value={value || ""}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChanges(e.target.value);
+        }}
+        type="text"
+        placeholder="asdf"
+      />
+    </div>
+  );
+};
+
+export const ColumnFilter = ({ column }) => {
+  const { filterValue, setFilter } = column;
+  return (
+    <div>
+      <input
+        value={filterValue || ""}
+        onChange={(e) => setFilter(e.target.value)}
+        type="text"
+        placeholder="asdf"
+      />
+    </div>
+  );
+};
 
 const TablePage = () => {
-    const [data] = useState(Mock_Data)
-    const columns = [
-        {
-            Header: 'ID',
-            accessor: 'id',
-            Footer: 'ID',
-            Cell: ((row) => {
-                return (
-                    <>
-                        <div className="dropdown">
-                            <div id="userdropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                                sdfsdfsd
-                            </div>
-                            <ul class="dropdown-menu" aria-labelledby="userdropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </div>
-
-                    </>
-                )
-            })
-        },
-        { Header: 'First Name', accessor: 'first_name', Footer: 'First Name', },
-        { Header: 'Last Name', accessor: 'last_name', Footer: 'Last Name', },
-        { Header: 'Email', accessor: 'email', Footer: 'Email', },
-        { Header: 'Gender', accessor: 'gender', Footer: 'Gender', },
-        { Header: 'IP Address', accessor: 'ip_address', Footer: 'IP Address', },
-    ]
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-8 border">
-                    <Table columns={columns} data={data} />
-                </div>
-            </div>
-
-
+  const [data] = useState(Mock_Data);
+  const columns = [
+    {
+      Header: "ID",
+      accessor: "id",
+      disableFilters: true,
+      Footer: "id",
+    },
+    { Header: "First Name", accessor: "first_name" },
+    { Header: "Last Name", accessor: "last_name" },
+    { Header: "Email", accessor: "email" },
+    { Header: "Gender", accessor: "gender" },
+    { Header: "IP Address", accessor: "ip_address" },
+  ];
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 border">
+          <Table
+            columns={columns}
+            globalFilterComponent={GlobalFilter}
+            columnFilterComponent={ColumnFilter}
+            data={data}
+            pagination={true}
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default TablePage
+export default TablePage;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,59 +1,164 @@
-import React from 'react'
-import { useTable } from 'react-table'
-import "./Table.styles.css"
+import React, { useMemo } from "react";
+import {
+  useTable,
+  useSortBy,
+  useGlobalFilter,
+  useFilters,
+  usePagination,
+} from "react-table";
+import "./Table.styles.css";
 
+export const ColumnFilter = ({ column }) => {
+  const { filterValue, setFilter } = column;
+  return (
+    <div>
+      <input
+        value={filterValue || ""}
+        onChange={(e) => setFilter(e.target.value)}
+        type="text"
+        placeholder="asdf"
+      />
+    </div>
+  );
+};
 const Table = (props) => {
-    const columns = props.columns
-    const data = props.data
-    const tableInstance = useTable({ columns, data },);
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, footerGroups } = tableInstance
-    return (
-        <div className="tableheighcalc">
-            <div className="table-scroll">
-                <table className="table border " {...getTableProps()}>
-                    <thead>
-                        {headerGroups.map((headerGroup) => {
-                            return (
-                                <tr  {...headerGroup.getHeaderGroupProps()}>
-                                    {headerGroup.headers.map((column) => {
-                                        return (
-                                            <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-                                        )
-                                    })}
-                                </tr>
-                            )
-                        })}
-                    </thead>
-                    <tbody {...getTableBodyProps()}>
-                        {rows.map((row, i) => {
-                            prepareRow(row)
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {row.cells.map((cell, i) => {
-                                        return (
-                                            <td {...cell.getCellProps()}> {cell.render('Cell')}</td>
-                                        )
-                                    })}
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                    <tfoot>
-                        {footerGroups.map((group) => (
-                            <tr {...group.getFooterGroupProps()}>
-                                {group.headers.map((column) => (
-                                    <td {...column.getFooterProps()}>
-                                        {column.render('Footer')}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tfoot>
-                </table>
-            </div>
+  const columns = props.columns;
+  const data = props.data;
+  const GlobalFilterComponent = props.globalFilterComponent;
+  const ColumnFilterComponent = props.columnFilterComponent || "";
+  const pagination = props.pagination;
+
+  const defaultColumn = useMemo(() => {
+    return { Filter: ColumnFilterComponent };
+  }, [ColumnFilterComponent]);
+
+  const tableInstance = useTable(
+    { columns, data, defaultColumn },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    page,
+    prepareRow,
+    footerGroups,
+    state,
+    setGlobalFilter,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+  } = tableInstance;
+
+  const { globalFilter, pageIndex } = state;
+  const haveFooterValues = footerGroups[0].headers.some(
+    (fG) => typeof fG["Footer"] === "string"
+  );
+
+  const tablerows = pagination ? page : rows;
+  return (
+    <>
+      {GlobalFilterComponent && (
+        <GlobalFilterComponent
+          filter={globalFilter}
+          setFilter={setGlobalFilter}
+        />
+      )}
+
+      <div className="tableheighcalc">
+        <div className="table-scroll">
+          <table className="table border " {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => {
+                return (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column) => {
+                      return (
+                        <th
+                          {...column.getHeaderProps(
+                            column.getSortByToggleProps()
+                          )}
+                        >
+                          {column.render("Header")}
+                          <span>
+                            {column.isSorted
+                              ? column.isSortedDesc
+                                ? "F"
+                                : "d"
+                              : ""}
+                          </span>
+                          <div>
+                            {column.canFilter ? column.render("Filter") : null}
+                          </div>
+                        </th>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {tablerows.map((row, i) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell, i) => {
+                      return (
+                        <td {...cell.getCellProps()}> {cell.render("Cell")}</td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+            {haveFooterValues && (
+              <tfoot>
+                {footerGroups.map((footergroup) => (
+                  <tr {...footergroup.getFooterGroupProps()}>
+                    {footergroup.headers.map((column) => (
+                      <td {...column.getFooterProps()}>
+                        {column.render("Footer")}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tfoot>
+            )}
+          </table>
         </div>
+      </div>
+      {pagination && (
+        <div>
+          <span>
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <button
+            onClick={() => previousPage()}
+            disabled={!canPreviousPage}
+            className="btn btn-success"
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => nextPage()}
+            disabled={!canNextPage}
+            className="btn btn-danger"
+          >
+            Next
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
 
-    )
-}
-
-export default React.memo(Table)
+export default React.memo(Table);
