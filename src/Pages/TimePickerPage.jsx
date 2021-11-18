8f879f025e14e70a6b889786dfd50b33b38d7e0e
Arslan Ahmed <ashi3610@gmail.com>
2021-11-18 23:06:42 +0500

    timepicker changes

--- src/Pages/TimePickerPage.jsx
+++ src/Pages/TimePickerPage.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TimePicker from "../components/TimePicker/TimePicker"
 
 const TimePickerPage = () => {
+    const [time, setTime] = useState("")
     const changeTime = (time) => {
         console.log("i run", time);
+        setTime(time)
     }
+
     return (
         <div className="container mt-5">
             <div className="row">
+                <div>
+                    <button className="btn btn-success" onClick={() => setTime("01:00 AM")}>change to 01:00 AM</button>
+                </div>
                 <div className="col-md-5 border p-5">
-                    <TimePicker changeTime={changeTime} timeFormat={12} />
+                    <TimePicker size="XS" time={time} changeTime={changeTime} timeFormat={12} />
                 </div>
 
             </div>
--- src/components/TimePicker/TimePicker.jsx
+++ src/components/TimePicker/TimePicker.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useRef, useMemo } from 'react'
 import "./TimePicker.css"
-import { FaAngleUp, FaAngleDown } from "react-icons/fa";
+import { FaRegClock } from "react-icons/fa";
 
 import { useNamedState } from "../../hooks/useNamedState"
 import useOutsideClick from "../../hooks/useOutsideClick"
+import Arrow_down from "./Arrow_down.svg"
+import Arrow_up from "./Arrow_up.svg"
 
 const TimePicker = (props) => {
     const timePickerRef = useRef();
+    const inputRef = useRef()
 
     const [showTimePicker, setShowTimePicker] = useNamedState("showTimePicker", false);
     const [hours, setHours] = useNamedState("hours", "12");
     const [minutes, setMinutes] = useNamedState("minutes", "00");
     const [period, setPeriod] = useNamedState("period", "AM");
-    const [time, setTime] = useNamedState("time", "12:00 AM");
+    const [time, setTime] = useNamedState("time", "12:00");
+    const [showError, setShowError] = useNamedState("showError", false)
+    const [styling, setStyling] = useNamedState("styling", {})
 
 
 
@@ -29,26 +34,71 @@ const TimePicker = (props) => {
     function parsedTime(time, unit, timeFormat) {
         if (unit === "h") {
             if (timeFormat === 12) {
-                let format12hr = { 1: "01", 2: "02", 3: "03", 4: "04", 5: "05", 6: "06", 7: "07", 8: "08", 9: "09", 10: "10", 11: "11", 12: "12", }
+                let format12hr = {};
+                for (var i = 1; i <= 12; i++) {
+                    switch (true) {
+                        case i <= 9:
+                            format12hr[i] = "0" + i;
+                            break;
+                        default:
+                            format12hr[i] = i.toString();
+                    }
+                }
+
                 if (time === 13) return format12hr[1]
                 if (time === 0) return format12hr[12]
                 return format12hr[time]
             }
             else {
-
-                let format24hr = { 1: "01", 2: "02", 3: "03", 4: "04", 5: "05", 6: "06", 7: "07", 8: "08", 9: "09", 10: "10", 11: "11", 12: "12", 13: "13", 14: "14", 15: "15", 16: "16", 17: "17", 18: "18", 19: "19", 20: "20", 21: "21", 22: "22", 23: "23", 24: "00", }
+                let format24hr = {};
+                for (var i = 1; i <= 24; i++) {
+                    switch (true) {
+                        case i <= 9:
+                            format24hr[i] = "0" + i;
+                            break;
+                        case i === 24:
+                            format24hr[i] = "00";
+                            break;
+                        default:
+                            format24hr[i] = i.toString();
+                    }
+                }
                 if (time === 0) return format24hr[24]
                 if (time === -1) return format24hr[23]
                 return format24hr[time]
             }
         }
         else {
-            let minutesObj = { 1: "01", 2: "02", 3: "03", 4: "04", 5: "05", 6: "06", 7: "07", 8: "08", 9: "09", 10: "10", 11: "11", 12: "12", 13: "13", 14: "14", 15: "15", 16: "16", 17: "17", 18: "18", 19: "19", 20: "20", 21: "21", 22: "22", 23: "23", 24: "25", 26: "26", 27: "27", 28: "28", 29: "29", 30: "31", 32: "32", 33: "33", 34: "34", 35: "35", 36: "36", 37: "37", 38: "39", 40: "40", 41: "41", 42: "42", 43: "43", 44: "45", 46: "46", 47: "47", 48: "48", 49: "49", 50: "50", 51: "51", 52: "52", 53: "53", 54: "54", 55: "55", 56: "56", 57: "57", 58: "58", 59: "59", 60: "00", }
+            let minutesObj = {};
+            for (var i = 1; i <= 60; i++) {
+                switch (true) {
+                    case i <= 9:
+                        minutesObj[i] = "0" + i;
+                        break;
+                    case i === 60:
+                        minutesObj[i] = "00";
+                        break;
+                    default:
+                        minutesObj[i] = i.toString();
+                }
+            }
             if (time === 0) return minutesObj[60]
             if (time === -1) return minutesObj[59]
             return minutesObj[time]
         }
+    }
 
+    const validateHhMm = (inputField, timeformat) => {
+        let isValid = null;
+        if (timeformat === 24) {
+            console.log("sdf");
+            isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField);
+        }
+        else {
+            isValid = /^([0]\d|[1][0-2]):([0-5]\d)\s?(?: AM|PM)$/i.test(inputField);
+        }
+
+        return isValid;
     }
 
 
@@ -99,10 +149,56 @@ const TimePicker = (props) => {
 
     // ************************** UPDATE TIME FUNCTON *****************
     const updateTime = () => {
-        setTime(() => {
-            return formatTime()
-        })
+        const isValid = validateHhMm(formatTime(), timeFormat)
+        if (isValid) {
+            console.log(isValid);
+            setTime(() => {
+                return formatTime()
+            })
+
+            setShowError(false)
+            inputRef.current.style.border = '1px solid #4f4f4f';
+            inputRef.current.value = formatTime()
+        }
+        else {
+            inputRef.current.style.border = '1px solid #f93154';
+            setShowError(true)
+        }
         setShowTimePicker(false);
+    }
+
+
+    // ************************** CHANGE TIME FUNCTION *****************
+    const handleChange = (e) => {
+        setTime(() => {
+            return e.target.value
+        })
+    }
+
+
+
+
+    // ************************** HANDLE BLUR FUNCTION *****************
+    const handleBlur = (e) => {
+        const isValid = validateHhMm(e.target.value, timeFormat)
+        if (isValid) {
+            setTime(() => {
+                return e.target.value
+            })
+            console.log("value", e.target.value);
+            const hours = e.target.value.substring(0, 2)
+            const minutes = e.target.value.substring(3, 5)
+            setHours(hours)
+            setMinutes(minutes)
+            setShowError(false)
+            inputRef.current.style.border = '1px solid #4f4f4f';
+        }
+        else {
+            console.log("else run");
+            inputRef.current.style.border = '1px solid #f93154';
+            setShowError(true)
+        }
+        //console.log("blur run", validateHhMm(e.target.value, timeFormat));
     }
 
     // ************************** USEOUTSIDECLICK HOOK ******************
@@ -115,47 +211,88 @@ const TimePicker = (props) => {
 
     // ************************** USEMEMO HOOK ************************
     const timeFormat = useMemo(() => props.timeFormat ? props.timeFormat : 12, [props.timeFormat]);
+    const size = useMemo(() => props.size ? props.size : "XS", [props.size]);
+    const newTime = useMemo(() => props.time ? props.time : "12:00 AM", [props.time]);
 
 
 
+    const updateStyling = () => {
+        let styleObject = {}
+        if (size === "XS") {
+            styleObject["okbtn_fontSize"] = "0.8rem"
+            styleObject["timepickerCurrent_fontSize"] = "1rem"
+            styleObject["iconup_top"] = "-3px"
+            styleObject["icondown_bottom"] = "-3px"
+        }
+        else if (size === "S") {
+            styleObject["okbtn_fontSize"] = "1.5rem"
+            styleObject["timepickerCurrent_fontSize"] = "1rem"
+            styleObject["iconup_top"] = "-3px"
+            styleObject["icondown_bottom"] = "-3px"
+        }
+        else {
+            styleObject["okbtn_fontSize"] = "0.5rem"
+            styleObject["timepickerCurrent_fontSize"] = "1rem"
+            styleObject["iconup_top"] = "-3px"
+            styleObject["icondown_bottom"] = "-3px"
+        }
+        console.log("updateSize run");
+        setStyling(styleObject)
+    }
 
 
-    // ************************** USE EFFECT FUNCTION ********************
+    // ************************** USE EFFECT HOOK ********************
     useEffect(() => {
-        props.changeTime(time)
+        props.changeTime(formatTime())
+
     }, [time, timeFormat])
+    useEffect(() => {
+        console.log("newTime", newTime);
+        setTime(() => {
+            return newTime
+        })
+        const hours = newTime.substring(0, 2)
+        const minutes = newTime.substring(3, 5)
+        inputRef.current.value = newTime
 
+        setHours(hours)
+        setMinutes(minutes)
+    }, [newTime])
 
+    useEffect(() => {
+        updateStyling()
+    }, [size])
     console.log("timePicker Render");
 
     return (
         <div className="form-outline"  >
-            <input type="text" value={time} className="timepicker-input" />
-            <button onClick={() => setShowTimePicker(!showTimePicker)} id="timepicker-toggle-371513" type="button" className="timepicker-toggle-button">😄</button>
+            <input type="text" ref={inputRef} onBlur={(e) => handleBlur(e)} className="timepicker-input" />
+            <button onClick={() => setShowTimePicker(!showTimePicker)} id="timepicker-toggle-371513" type="button" className="timepicker-toggle-button"><FaRegClock /></button>
+            {showError && (<div className="errordiv">Invalid Time Format</div>)}
             {showTimePicker && (
-                <div className="timepicker-wrapper" ref={timePickerRef}>
+                <div className="timepicker-wrapper" ref={timePickerRef}    >
                     <div className="row h-100 w-100  g-0 justify-content-center align-items-center">
                         <div className="col-6">
                             <div className="row g-0">
-                                <div className="col-5 d-flex justify-content-center align-items-center position-relative  ">
-                                    <div className="timepicker_wrapper_icon timepicker_wrapper_icon_up">
-                                        <FaAngleUp className="timepicker_icon" onClick={() => changeHours("ADD")} />
+                                <div className="col-5  d-flex justify-content-center align-items-center position-relative  ">
+                                    <div className="timepicker_wrapper_icon" style={{ top: styling.iconup_top }}>
+                                        <img src={Arrow_up} style={{ width: "12px" }} className="timepicker_icon" alt="arrow_up" onClick={() => changeHours("ADD")} />
                                     </div>
-                                    <button className="timepicker-current">{hours}</button>
-                                    <div className="timepicker_wrapper_icon timepicker_wrapper_icon_down" >
-                                        <FaAngleDown className="timepicker_icon" onClick={() => changeHours("MINUS")} />
+                                    <button className="timepicker-current" style={{ fontSize: styling.timepickerCurrent_fontSize }}>{hours}</button>
+                                    <div className="timepicker_wrapper_icon" style={{ bottom: styling.icondown_bottom }} >
+                                        <img src={Arrow_down} style={{ width: "12px" }} alt="arrow_down" onClick={() => changeHours("MINUS")} className="timepicker_icon" />
                                     </div>
                                 </div>
-                                <div className="col-2 d-flex  justify-content-center align-items-center">
+                                <div className="col-2  d-flex  justify-content-center align-items-center">
                                     <p className="timepicker-dots">:</p>
                                 </div>
-                                <div className="col-5 d-flex justify-content-center align-items-center position-relative  " >
-                                    <div className="timepicker_wrapper_icon timepicker_wrapper_icon_up">
-                                        <FaAngleUp className="timepicker_icon" onClick={() => changeMinutes("ADD")} />
+                                <div className="col-5  d-flex justify-content-center align-items-center position-relative  " >
+                                    <div className="timepicker_wrapper_icon" style={{ top: styling.iconup_top }}>
+                                        <img src={Arrow_up} style={{ width: "12px" }} className="timepicker_icon" alt="arrow_up" onClick={() => changeMinutes("ADD")} />
                                     </div>
                                     <button className="timepicker-current">{minutes}</button>
-                                    <div className="timepicker_wrapper_icon timepicker_wrapper_icon_down" >
-                                        <FaAngleDown className="timepicker_icon" onClick={() => changeMinutes("MINUS")} />
+                                    <div className="timepicker_wrapper_icon" style={{ bottom: styling.icondown_bottom }} >
+                                        <img src={Arrow_down} style={{ width: "12px" }} alt="arrow_down" onClick={() => changeMinutes("MINUS")} className="timepicker_icon" />
                                     </div>
                                 </div>
                             </div>
@@ -163,19 +300,19 @@ const TimePicker = (props) => {
                         <div className="col-6">
                             <div className="row justify-content-end g-0">
                                 {timeFormat === 12 && (
-                                    <div className="col-5  d-flex justify-content-center align-items-center position-relative  ">
-                                        <div className="timepicker_wrapper_icon timepicker_wrapper_icon_up">
-                                            <FaAngleUp className="timepicker_icon" onClick={changePeriod} />
+                                    <div className="col-5   d-flex justify-content-center align-items-center position-relative  ">
+                                        <div className="timepicker_wrapper_icon" style={{ top: styling.iconup_top }}>
+                                            <img src={Arrow_up} style={{ width: "12px" }} className="timepicker_icon" alt="arrow_up" onClick={changePeriod} />
                                         </div>
-                                        <button className="timepicker-current">{period}</button>
-                                        <div className="timepicker_wrapper_icon timepicker_wrapper_icon_down" >
-                                            <FaAngleDown className="timepicker_icon" onClick={changePeriod} />
+                                        <button className="timepicker-current" style={{ fontSize: styling.timepickerCurrent_fontSize }}>{period}</button>
+                                        <div className="timepicker_wrapper_icon " style={{ bottom: styling.icondown_bottom }} >
+                                            <img src={Arrow_down} style={{ width: "12px" }} alt="arrow_down" onClick={changePeriod} className="timepicker_icon" />
                                         </div>
                                     </div>
                                 )}
 
-                                <div className="col-3 d-flex justify-content-center align-items-center  ">
-                                    <button className="ok_btn" onClick={updateTime} >OK</button>
+                                <div className="col-3  d-flex justify-content-center align-items-center  ">
+                                    <button className="ok_btn" style={{ fontSize: styling.okbtn_fontSize }} onClick={updateTime} >OK</button>
                                 </div>
                             </div>
                         </div>
@@ -188,4 +325,4 @@ const TimePicker = (props) => {
     )
 }
 
-export default TimePicker
+export default React.memo(TimePicker)
